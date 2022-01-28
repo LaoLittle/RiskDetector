005f0d2afae8bf24fa9c7cf30f505a1068ff09d2
LaoLittle <y1941803875@outlook.com>
2022-01-28 16:52:31 +0800

    删去`ping`

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,10 +1,8 @@
 package org.laolittle.plugin
 
-import java.net.InetAddress
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
-import kotlin.system.measureTimeMillis
 
 
 @OptIn(ExperimentalContracts::class)
@@ -16,10 +14,4 @@ internal inline fun <R> retryWhenFailed(limit: Int, block: () -> R): R? {
         runCatching { return block() }.onFailure { RiskDetector.logger.error(it) }
     }
     return null
-}
-
-internal fun ping(ipAddress: String = "www.qq.com"): Long {
-    return measureTimeMillis {
-        InetAddress.getByName(ipAddress).isReachable(3000)
-    }
 }
